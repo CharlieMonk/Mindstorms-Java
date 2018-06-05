21b12852a6bcaff0bb889c4a466863f691f595c3
Charlie Monk <CharlieMonk@users.noreply.github.com>
2018-06-05 11:07:20 -0400

    New stuff

--- Main.java
+++ Main.java
@@ -1,26 +1,36 @@
-import lejos.nxt.UltrasonicSensor;
-import lejos.nxt.LightSensor;
-import lejos.nxt.Motor;
-
-public static void main(String args[]){
-    // Fill these in with ultrasonic and light ports!
-    ultraPort = 0;
-    lightPort = 0;
-
-    UltrasonicSensor ultra = new UltrasonicSensor(ultraPort);
-    ultra.continuous();
-    LightSensor light = new LightSensor(lightPort);
-
-    while(true){
-        int dist = ultra.getDistance();
-        int lightAmt = light.getLightValue();
-        if(dist != 255 && lightAmt < 90){
-            // don't do anything if you can't see anything or are on edge!
-            // (ultra fake val is 255, >90 light is surely white)
-            Motor.A.setSpeed(720);
-            Motor.B.setSpeed(720);
-
-        }
-
+import org.jabotics.robot.en.*;
+public class Main extends XRobot{
+    
+    static final IMotor leftMotor = objectFactory.createMotor(EMotorPort.C);
+    static final IMotor rightMotor = objectFactory.createMotor(EMotorPort.B);
+    static final IPilot pilot = objectFactory.createPilot(EMotorPort.C, EMotorPort.B);
+    static final ITouchSensor enterButton = objectFactory.createTouchSensor(EButton.ENTER);
+    static final ITouchSensor escapeButton = objectFactory.createTouchSensor(EButton.ESCAPE);
+    static final ITouchSensor leftButton = objectFactory.createTouchSensor(EButton.LEFT);
+    static final ITouchSensor rightButton = objectFactory.createTouchSensor(EButton.RIGHT);
+    static final ITouchSensor touchSensor = objectFactory.createTouchSensor(ESensorPort.S1);
+    static final ISensor soundSensor = objectFactory.createSoundSensor(ESensorPort.S2);
+    static final ILightSensor lightSensor = objectFactory.createLightSensor(ESensorPort.S3);
+    static final ISensor ultrasonicSensor = objectFactory.createUltrasonicSensor(ESensorPort.S4);
+    static final ITimer timer = objectFactory.createTimer();
+    static final IDisplay display = objectFactory.createDisplay();
+    static final ISpeaker speaker = objectFactory.createSpeaker();
+    public static void Main(String[] args){
+        timer.wait(2000); // time to get out of the way after starting the program
+        pilot.resetCounters();
+        pilot.setTargetSpeed(10);
+        pilot.moveStraight(FORWARD);
+        ultrasonicSensor.waitUntilBelowLimit(50);
+        pilot.stopMotion(WAIT);
+        float distance = pilot.getTachoCount(); // tacho count is being stored in new float variable "distance"
+        speaker.playBeepSequence();
+        display.clear();
+        display.write("Distance in cm:", 0, 0);
+        display.write(distance, 0, 1);
+        display.write("Press", 1, 3);
+        display.write("escape", 2, 4);
+        display.write("button!", 3, 5);
+        escapeButton.waitForReleaseEvent();
     }
+    
 }
---
+++ test.java
@@ -0,0 +1,62 @@
+
+
+import org.jabotics.robot.en.*;
+
+/**
+ * Describe your class test here.
+ * 
+ * @author  (your name) 
+ * @version (a version number or date)
+ */
+public class test extends XRobot
+{
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Definition of all objects that are necessary to control the robot in standard configuration
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    static final IMotor leftMotor = objectFactory.createMotor(EMotorPort.C);
+    static final IMotor rightMotor = objectFactory.createMotor(EMotorPort.B);
+    //static final IMotor sideMotor = objectFactory.createMotor(EMotorPort.A);
+    static final IPilot pilot = objectFactory.createPilot(EMotorPort.B, EMotorPort.C);
+    //static final IPilot pilot2 = objectFactory.createPilot(EMotorPort.A);
+    static final ITouchSensor enterButton = objectFactory.createTouchSensor(EButton.ENTER);
+    static final ITouchSensor escapeButton = objectFactory.createTouchSensor(EButton.ESCAPE);
+    //static final ISensor soundSensor = objectFactory.createSoundSensor(ESensorPort.S2);
+    static final ILightSensor lightSensor = objectFactory.createLightSensor(ESensorPort.S1);
+    static final ISensor ultrasonicSensor = objectFactory.createUltrasonicSensor(ESensorPort.S2);
+    static final ITimer timer = objectFactory.createTimer();
+    static final IDisplay display = objectFactory.createDisplay();
+    static final ISpeaker speaker = objectFactory.createSpeaker();
+    
+     /**
+     * The methode "main" (the "main program") is the starting point for the robot program.
+     * In this method you will implement your own robot control.
+     * Replace the sample code by your own instructions.
+     */
+    public static void main(String[] args)
+    {
+        boolean inside = true;
+        pilot.resetCounters();
+          
+        lightSensor.switchLightOn();
+        pilot.moveStraight(FORWARD);
+        while(true){
+            if(ultrasonicSensor.getMeasuredValue() < 50){
+                pilot.setTargetSpeedToMax();
+                if(lightSensor.getMeasuredValue() < 40){
+                    pilot.setTargetSpeed(0);
+                    break;
+                }
+            }
+        }
+
+    
+    
+        //sideMotor.setTargetSpeedToMax();
+        // ultrasonicSensor.waitUntilBelowLimit(50);
+        // pilot.setTargetSpeed(0);
+        //display.clear();
+        //display.write("Distance in cm:", 0, 0);
+
+        escapeButton.waitForReleaseEvent();
+    }
+}
